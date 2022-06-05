e77091d00341aa3144d4738d0fceba9575abdde0
Vlad <vladkozlov2012@mail.ru>
2022-06-05 16:10:13 +0300

    Add Interfaces/ILSASettings.cs

---
+++ Interfaces/ILSASettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODDating.Interfaces
+{
+    interface ILSAZPSettings
+    {
+        int AmountMoves { get; set; }
+        int MovePause { get; set; }
+        int SessionPause { get; set; }
+    }
+}
