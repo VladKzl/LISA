7144f3b54fd02fdb3a4e2a85fda7cadaf0078d41
Vlad <vladkozlov2012@mail.ru>
2022-06-15 17:47:29 +0300

    Add MyNpg/StaticNpg

---
+++ MyNpg/StaticNpg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNpg
+{
+    public static class StaticNpg
+    {
+        public static DataSet DataSet { get; set; }
+        public static DataTable Main { get; set; }
+        public static DataTable Groups { get; set; }
+    } 
+}
