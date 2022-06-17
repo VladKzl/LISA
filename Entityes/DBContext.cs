24b7b99830ccff68fdb804fcc949e2bf05763cbb
Vlad <vladkozlov2012@mail.ru>
2022-06-17 22:43:55 +0300

    Add Entityes/DBContext.cs

---
+++ Entityes/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODDating.Entityes
+{
+    public static class DBContext
+    {
+        public static DataSet DataSet { get; set; }
+        public static DataTable Main { get; set; }
+        public static DataTable Groups { get; set; }
+    } 
+}
